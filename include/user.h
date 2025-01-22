5cd709611f49eac8d020da15fe17f605ff457637
Luka <lzivoder@vub.hr>
2025-01-22 02:43:46 +0100

    base user class

---
+++ include/user.h
@@ -0,0 +1,13 @@
+#include <string>
+
+class User {
+protected:
+    std::string username;
+    std::string password;
+
+public:
+    User(std::string user, std::string pass);
+    virtual bool login(std::string user, std::string pass);
+    virtual ~User();
+    std::string getUsername();
+};
---
+++ src/user.cpp
@@ -0,0 +1,13 @@
+#include "user.h"
+
+User::User(std::string user, std::string pass) : username(user), password(pass) {}
+
+bool User::login(std::string user, std::string pass) {
+    return (user == username && pass == password);
+}
+
+User::~User() {}
+
+std::string User::getUsername() {
+    return username;
+}
